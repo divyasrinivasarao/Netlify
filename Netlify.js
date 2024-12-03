d885768686b09592b1f004262d42cc36387d974d
divyasrinivasarao <159988297+divyasrinivasarao@users.noreply.github.com>
2024-12-03 17:21:03 +0530

    Add files via upload

---
+++ Netlify.js
@@ -0,0 +1,9 @@
+// Add smooth scrolling for navigation
+document.querySelectorAll('a.nav-link').forEach(link => {
+    link.addEventListener('click', e => {
+        e.preventDefault();
+        document.querySelector(link.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
